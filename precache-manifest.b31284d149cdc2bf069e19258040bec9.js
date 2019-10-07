e5e7d065064cd7a67e7d2b56b104d87ee27c4542
nikolai <nikolai@theministry.co>
2019-10-07 16:47:50 +0200

    Updates

---
+++ precache-manifest.b31284d149cdc2bf069e19258040bec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d93ed896e919b841027604406dafdd",
+    "url": "/propaganda/index.html"
+  },
+  {
+    "revision": "10f4c91406dc1347cfd0",
+    "url": "/propaganda/static/js/2.cb0f4100.chunk.js"
+  },
+  {
+    "revision": "1bef18787fcae712f5be",
+    "url": "/propaganda/static/js/main.68432370.chunk.js"
+  },
+  {
+    "revision": "9e1f6d213b7ea2221951",
+    "url": "/propaganda/static/js/runtime-main.42408079.js"
+  },
+  {
+    "revision": "db6a66f7f19e1b9a09eab7782645990c",
+    "url": "/propaganda/static/media/colorbar.db6a66f7.svg"
+  }
+]);
